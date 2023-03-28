851f839d0d01e28fee10c737a247a42199368004
ujjwol thapa <ujjwolthapa0@gmail.com>
2023-03-28 09:13:49 +0545

    testing

--- backend/settings.py
+++ backend/settings.py
@@ -192,13 +192,24 @@ CORS_ALLOW_ALL_ORIGINS = True
 #     }  
 # }  
 # postgres
+import os
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'ecommerce',
+#        'USER': 'postgres',
+#        'PASSWORD': 'admin',
+#        'HOST': 'db',
+#        'PORT': '5432',
+#    }
+# }
 DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.postgresql',
-       'NAME': 'ecommerce',
-       'USER': 'postgres',
-       'PASSWORD': 'admin',
-       'HOST': 'db',
-       'PORT': '5432',
-   }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': os.environ.get('DATABASE_HOST', 'localhost'),
+        'PORT': os.environ.get('DATABASE_PORT', '5432'),
+        'NAME': os.environ.get('DATABASE_NAME', 'postgres'),
+        'USER': os.environ.get('DATABASE_USER'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+    }
 }
